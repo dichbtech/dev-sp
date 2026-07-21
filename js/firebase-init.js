74511e1ab4be16e7a66b71546714090aa5019e92
dichbtech <dicsupervisores@gmail.com>
2026-07-21 19:41:35 -0300

    fix: aplica posicionamentos oficiais do layout de metas e pódio

--- js/firebase-init.js
+++ js/firebase-init.js
@@ -29,20 +29,23 @@ window.cargosMap = {};
 window.isEditMode = false;
 
 window.layoutConfig = {
-    'img-podio-base': { left: '100px', top: '150px', width: 'auto' },
-    'avatar-1': { left: '175px', top: '50px', width: 'auto' },
-    'medal-1': { left: '175px', top: '0px', width: 'auto' },
-    'nick-1': { left: '160px', top: '120px', width: 'auto' },
-    'avatar-2': { left: '80px', top: '100px', width: 'auto' },
-    'medal-2': { left: '80px', top: '50px', width: 'auto' },
-    'nick-2': { left: '60px', top: '170px', width: 'auto' },
+    'avatar-1': { left: '114px', top: '34px', width: '124px' },
+    'avatar-2': { left: '176px', top: '100px', width: '124px' },
     'avatar-empate-1': { left: '175px', top: '50px', width: 'auto' },
-    'txt-empate-1': { left: '175px', top: '20px', width: 'auto' },
     'avatar-empate-2': { left: '80px', top: '100px', width: 'auto' },
-    'txt-empate-2': { left: '80px', top: '70px', width: 'auto' },
-    'img-palco-base': { left: '50px', top: '220px', width: 'auto' },
-    'img-brasao': { left: '10px', top: '10px', width: 'auto' },
-    'img-emblema-sup': { left: '45px', top: '40px', width: 'auto' },
-    'avatar-selecionado': { left: '130px', top: '100px', width: 'auto' },
-    'sponsorInner': { left: '0px', top: '-10px', width: '60px' }
+    'avatar-selecionado': { left: '104px', top: '59px', width: '154px' },
+    'img-brasao': { left: '-7px', top: '47px', width: '181px' },
+    'img-emblema-sup': { left: '38px', top: '93px', width: '84px' },
+    'img-palco-base': { left: '102px', top: '255px', width: '170px' },
+    'img-podio-base': { left: '87px', top: '203px', width: '252px' },
+    'img-premio-hc': { width: '40px' },
+    'img-premio-moedas': { width: '100px' },
+    'img-premio-moedas-0': { width: '175px' },
+    'medal-1': { left: '156px', top: '32px', width: 'auto' },
+    'medal-2': { left: '219px', top: '98px', width: 'auto' },
+    'nick-1': { left: '25px', top: '154px', width: 'auto' },
+    'nick-2': { left: '304px', top: '201px', width: 'auto' },
+    'sponsorInner': { left: '-5px', top: '-16px', width: '60px' },
+    'txt-empate-1': { left: '139px', top: '49px', width: 'auto' },
+    'txt-empate-2': { left: '204px', top: '124px', width: 'auto' }
 };
